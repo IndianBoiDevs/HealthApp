60c67a101e24597e7ce4729d85fa4326436a7f7d
PrismaGaming123 <arnoldshibu2002@gmail.com>
2022-12-27 16:34:24 -0700

    Task12 Create the add functionality front end

---
+++ src/main/java/controllers/AddPopUpController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+
+public class AddPopUpController {
+    public void savePressed(ActionEvent event) {
+    }
+
+    public void cancelPressed(ActionEvent event) {
+    }
+}
